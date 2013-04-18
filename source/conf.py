adffdcfadc7e62baba59e2af0d5278190d5a15bc
Alexei Boronine <alexei.boronine@gmail.com>
2013-04-18 18:07:40 -0400

    Use theme_options instead.

--- source/conf.py
+++ source/conf.py
@@ -96,14 +96,12 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "github_url": "https://github.com/cosmic-api/teleport-docs"
+}
 
 # This is necessary for Cosmic site builder. Keep it here.
 html_theme_path = ["../../sphinx-bootstrap"]
-
-html_context = {
-    "github_url": "https://github.com/cosmic-api/teleport-docs"
-}
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".

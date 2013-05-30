4e9b5dc7498242f2b4ffaf1878d91be75fa8f1f4
Alexei Boronine <alexei.boronine@gmail.com>
2013-05-30 06:45:31 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst") as readme:
 
 setup(
     name = 'teleport',
-    version = "0.0.8-dev",
+    version = "0.1.0",
     py_modules = ['teleport'],
     description = 'An extandable serialization system',
     license = "MIT",

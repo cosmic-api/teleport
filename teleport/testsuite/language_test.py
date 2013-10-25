60ecad4865931bd3b6a7a7615e7df48635921bce
Alexei Boronine <alexei.boronine@gmail.com>
2013-10-25 13:31:08 -0400

    Add more cross-language tests

--- teleport/testsuite/language_test.py
+++ teleport/testsuite/language_test.py
@@ -34,16 +34,29 @@ def t(schema, all=None, passing=None, failing=None):
 
 def make_json_suite():
     tests = [
-        t(Integer, all=primitives, passing=[Box(-1), Box(1), Box(1.0)]),
-        t(Boolean, all=primitives, passing=[Box(True), Box(False)]),
-        t(String, all=primitives, passing=[Box("")]),
-        t(Float, all=primitives, passing=[Box(-1), Box(1), Box(1.0), Box(1.1)]),
+        t(Integer,
+            all=primitives,
+            passing=[Box(-1), Box(1), Box(1.0)]),
+        t(Boolean,
+            all=primitives,
+            passing=[Box(True), Box(False)]),
+        t(String,
+            all=primitives,
+            passing=[Box("")]),
+        t(Float,
+            all=primitives,
+            passing=[Box(-1), Box(1), Box(1.0), Box(1.1)]),
         t(DateTime,
             failing=primitives + [Box("2013-10-04 13:05:25.354952")], # Needs a T
-            passing=[Box("2013-10-04T13:05:25.354952")])
+            passing=[Box("2013-10-04T13:05:25.354952")]),
+        t(Array(Integer),
+            passing=[Box([]), Box([1]), Box([1.0])],
+            failing=set(primitives + [Box([1.1])]) - set([Box([])])),
+        t(Map(Integer),
+            passing=[Box({}), Box({"a": 1}), Box({"a": 1.0})],
+            failing=set(primitives) - set([Box({})])),
     ]
     # Binary
-    # DateTime
     # Array
     # Struct
     # Map
@@ -67,9 +80,11 @@ def suite():
     suite = TestSuite()
     for test in make_json_suite():
         for p in test['pass']:
-            suite.addTest(make_pass(test['schema'], p.datum))
+            datum = json.loads(json.dumps(p.datum))
+            suite.addTest(make_pass(test['schema'], datum))
         for f in test['fail']:
-            suite.addTest(make_fail(test['schema'], f.datum))
+            datum = json.loads(json.dumps(f.datum))
+            suite.addTest(make_fail(test['schema'], datum))
     return suite
 
 if __name__ == "__main__":

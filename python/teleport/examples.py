684f368ace1c80094196dc05c6d0b19c72d924ad
Alexei Boronine <alexei@boronine.com>
2014-11-27 05:12:17 +0800

    Implement Nullable

--- python/teleport/examples.py
+++ python/teleport/examples.py
@@ -1,6 +1,6 @@
 import re
 
-from teleport import Teleport, ConcreteType
+from teleport import Teleport, ConcreteType, GenericType
 
 t = Teleport()
 
@@ -11,3 +11,15 @@ class ColorType(ConcreteType):
         if not t("String").contains(value):
             return False
         return re.compile('^#[0-9a-f]{6}$').match(value) is not None
+
+
+@t.register("Nullable")
+class NullableType(GenericType):
+
+    def process_param(self, param):
+        self.child = self.t(param)
+
+    def contains(self, value):
+        if value is None:
+            return True
+        return self.child.contains(value)
--- python/teleport/testsuite/test_custom_types.py
+++ python/teleport/testsuite/test_custom_types.py
@@ -2,10 +2,14 @@ from unittest2 import TestCase
 from teleport.examples import t
 
 
-class ExtendingTest(TestCase):
+class CustomTypeTest(TestCase):
 
-    def test_extend(self):
+    def test_Color(self):
         self.assertTrue(t("Color").contains('#ffffff'))
         self.assertFalse(t("Color").contains('yellow'))
         self.assertTrue(t({"Array": "Color"}).contains(['#ffffff', '#000000']))
 
+    def test_Nullable(self):
+        self.assertTrue(t({"Nullable": "Integer"}).contains(1))
+        self.assertTrue(t({"Nullable": "Integer"}).contains(None))
+        self.assertFalse(t({"Nullable": "Integer"}).contains(True))

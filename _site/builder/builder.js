bc5cff0e97abcc55734a2b62fe84b9d1da350a49
Alexei Boronine <alexei.boronine@gmail.com>
2016-04-02 02:09:46 -0400

    Builder improvements

--- _site/builder/builder.js
+++ _site/builder/builder.js
@@ -2,18 +2,9 @@
 
 var os = require("os");
 var fs = require("fs");
+var glob = require("glob");
 var crypto = require("crypto");
 
-
-var stringHash = function (str) {
-    var hash = 5381;
-    var i = str.length;
-    while (i) {
-        hash = (hash * 33) ^ str.charCodeAt(--i);
-    }
-    hash = hash >>> 0;
-    return ("00000000" + hash.toString(8)).substr(-8);
-};
 
 
 var normalizeCommands = function (commands) {
@@ -71,7 +62,17 @@ class Makefile {
         targetNames.sort();
 
         for (let name of targetNames) {
-            s += this.targets[name].stringify() + "\n\n";
+            let target = this.targets[name];
+            let commands = target.getCommands(this.rootDir);
+            if (commands.length > 0) {
+                s += target.getFilename() + ": ";
+                s += target.getStringDeps().join(' ') + "\n\t";
+                s += "mkdir -p build\n\t";
+                s += target.getCommands(this.rootDir).join("\n\t");
+                s += "\n";
+            }
+
+            s += "\n"
         }
 
         return s;
@@ -99,8 +100,18 @@ class Target {
         return this.options.staticDeps || [];
     }
 
+    getStringDeps() {
+        const stringDeps = [].concat(this.getStaticDeps());
+        for (let depRule of this.getDeps()) {
+            stringDeps.push(depRule.getFilename());
+        }
+        stringDeps.sort();
+        return stringDeps;
+    }
+
     getStringDepsRecursive() {
         var allDeps = new Set(this.getStaticDeps());
+
         for (let dep of this.getDeps()) {
             allDeps.add(dep.getFilename());
             for (let subDep of dep.getStringDepsRecursive()) {
@@ -108,34 +119,6 @@ class Target {
             }
         }
         return Array.from(allDeps);
-    }
-
-    stringify() {
-        const stringDeps = [].concat(this.getStaticDeps());
-        for (let depRule of this.getDeps()) {
-            stringDeps.push(depRule.getFilename());
-        }
-        stringDeps.sort();
-
-        var s = this.getFilename() + ": ";
-        s += stringDeps.join(' ') + "\n\t";
-        s += this.getCommands().join("\n\t");
-        return s;
-    }
-}
-
-class Dist extends Target {
-    constructor(name, src) {
-        var distDir = `dist/${name}`;
-        super({
-            filename: distDir,
-            deps: [src],
-            commands: normalizeCommands(`
-                rm -rf ${distDir}
-                mkdir -p ${distDir}
-                tar xf ${src.getFilename()} -C ${distDir}
-            `)
-        });
     }
 }
 
@@ -150,27 +133,21 @@ class BuildTarget extends Target {
         return allDeps;
     }
 
-    getHash() {
-        var salt = this.options.salt || '';
-        var allDeps = this.getStringDepsRecursive();
-        allDeps.sort();
-        return stringHash(allDeps.join(' ') + salt);
-    }
-
     getFilename() {
-        return `build/${this.getHash()}.tar`;
+        var archive = this.options.archive;
+        return `build/${archive}.zip`;
     }
 
     getMounts() {
         return this.options.mounts || {};
     }
 
-    getCommands() {
+    getCommands(rootDir) {
         var resultDir = this.options.resultDir || "/";
         var mounts = this.getMounts();
         var filename = this.getFilename();
 
-        var tmp = `build/${this.getHash()}`;
+        var tmp = `build/${this.options.archive}`;
         var commands = [
             `rm -rf ${tmp}`,
             `mkdir -p ${tmp}`
@@ -178,10 +155,10 @@ class BuildTarget extends Target {
 
         for (let root of Object.keys(mounts)) {
             commands.push(`mkdir -p ${tmp}${root}`);
-            commands.push(`tar xf ${mounts[root].getFilename()} -C ${tmp}${root}`);
+            commands.push(`unzip -q ${mounts[root].getFilename()} -d ${tmp}${root}`);
         }
         commands = commands.concat(normalizeCommands(this.options.getCommands(tmp)));
-        commands.push(`tar cf ${filename} -C ${tmp}${resultDir} .`);
+        commands.push(`(cd ${tmp}${resultDir}; zip -q -r ${rootDir}/${filename} .)`);
         commands.push(`rm -rf ${tmp}`);
         return commands;
     }
@@ -190,52 +167,162 @@ class BuildTarget extends Target {
 var googleFonts = function (googleUrl) {
     return new BuildTarget({
         resultDir: "/out",
+        archive: 'google-fonts',
         getCommands: function (tmp) {
             return `
-            wget -O ${tmp}/index.css "${googleUrl}"
-            cat ${tmp}/index.css | grep -o -e "http.*ttf" > ${tmp}/download.list
-            (cd ${tmp} && wget -i download.list)
-            mkdir ${tmp}/out
-            cp ${tmp}/*.ttf ${tmp}/out
-            sed 's/http.*\\/\\(.*\\.ttf\\)/\"..\\/fonts\\/\\1\"/g' < ${tmp}/index.css > ${tmp}/out/index.css
+                wget -O ${tmp}/index.css "${googleUrl}"
+                cat ${tmp}/index.css | grep -o -e "http.*ttf" > ${tmp}/download.list
+                (cd ${tmp} && wget -i download.list)
+                mkdir ${tmp}/out
+                cp ${tmp}/*.ttf ${tmp}/out
+                sed 's/http.*\\/\\(.*\\.ttf\\)/\"..\\/fonts\\/\\1\"/g' < ${tmp}/index.css > ${tmp}/out/index.css
             `;
         }
     });
 };
 
-var fileDownload = function (options) {
-    var filename = options.filename;
-    var url = options.url;
+var zipFileDownload = function (archive, url) {
+    return new Target({
+        filename: `build/${archive}.zip`,
+        commands: [`wget -O build/${archive}.zip "${url}"`]
+    });
+};
 
+var fileDownload = function (filename, url) {
     return new BuildTarget({
-        salt: url,
+        archive: `download-${filename}`,
         getCommands: function (tmp) {
             return `wget -O ${tmp}/${filename} "${url}"`;
         }
     });
 };
 
-var tarFromZip = function (url) {
+var zipFile = function (archive, filename) {
+    return new Target({
+        filename: filename,
+        archive: archive,
+        staticDeps: [],
+        commands: []
+    });
+};
+
+var pythonVenv = function (version) {
     return new BuildTarget({
-        resultDir: "/out",
-        salt: url,
+        archive: `venv-${version}`,
         getCommands: function (tmp) {
             return `
-            wget ${url} -O ${tmp}/temp.zip
-            mkdir ${tmp}/out
-            unzip ${tmp}/temp.zip -d ${tmp}/out
+                python${version} -m venv --without-pip ${tmp}
+                wget https://bootstrap.pypa.io/get-pip.py -P ${tmp}
+                ${tmp}/bin/python ${tmp}/get-pip.py
+                ${tmp}/bin/pip install sphinx
             `;
         }
     });
 };
 
+var pythonDocs = function (version, venv) {
+    var staticDeps = glob.sync(`python/${version}/docs/**`);
+    staticDeps = staticDeps.concat(glob.sync(`python/${version}/teleport/**`));
+    staticDeps = staticDeps.concat(`python/${version}/CHANGES.rst`);
+
+    return new BuildTarget({
+        resultDir: "/python/out",
+        staticDeps: staticDeps,
+        archive: `docs-python-${version}`,
+        mounts: {
+            "/flask-sphinx-themes": zipFileDownload(
+                "flask-sphinx-themes.zip",
+                "https://github.com/cosmic-api/flask-sphinx-themes/archive/master.zip"),
+            "/intersphinx/python2": fileDownload(
+                "python2.inv",
+                "https://docs.python.org/2.7/objects.inv"),
+            "/venv": venv
+        },
+        getCommands: function (tmp) {
+            return `
+                cp -R python/${version} ${tmp}/python
+                cp ${tmp}/intersphinx/python2/python2.inv ${tmp}/python/docs
+                echo '\\nhtml_theme_path = ["../../flask-sphinx-themes/flask-sphinx-themes-master"]\\n' >> ${tmp}/python/docs/conf.py
+                echo '\\nintersphinx_mapping = {"python": ("http://docs.python.org/2.7", "python2.inv")}\\n' >> ${tmp}/python/docs/conf.py
+                (cd ${tmp}/python; ../venv/bin/python setup.py install)
+                (cd ${tmp}/python; ../venv/bin/python ../venv/bin/sphinx-build -b html -D html_theme=flask docs out)
+            `;
+        }
+    });
+};
+
+var formatSpec = function (version) {
+    return new BuildTarget({
+        archive: `spec-${version}`,
+        staticDeps: [
+            "_site/spec.js",
+            "_site/templates/spec.mustache",
+            `_spec/${version}.xml`
+        ],
+        resultDir: "/out",
+        getCommands: function (tmp) {
+            return `
+                xml2rfc --no-network _spec/${version}.xml --text --out=${tmp}/teleport.txt
+                mkdir ${tmp}/out
+                node _site/spec.js < ${tmp}/teleport.txt > ${tmp}/out/index.html
+            `;
+        }
+    });
+};
+
+var inject = function (source, args) {
+    if (!source.options.archive) {
+        console.log(source);
+    }
+    return new BuildTarget({
+        archive: `${source.options.archive}-inject`,
+        staticDeps: ["_site/inject.js", "_site/templates/navbar.mustache"],
+        mounts: {
+            "/": source
+        },
+        getCommands: function (tmp) {
+            return `find ${tmp} -iname \\*.html | xargs node _site/inject.js ${args}`;
+        }
+    });
+};
+
+var localNpmPackage = function (name) {
+    return new BuildTarget({
+        archive: `npm-${name}`,
+        staticDeps: [`node_modules/${name}/package.json`],
+        getCommands: function (tmp) {
+            return `cp -R node_modules/${name}/* ${tmp}`;
+        }
+    });
+};
+
+var dist = function(name, src) {
+    var distDir = `dist/${name}`;
+    return new Target({
+        filename: distDir,
+        deps: [src],
+        commands: normalizeCommands(`
+            rm -rf ${distDir}
+            mkdir -p ${distDir}
+            tar xf ${src.getFilename()} -C ${distDir}
+        `)
+    });
+};
+
+
 module.exports = {
     normalizeCommands: normalizeCommands,
     Makefile: Makefile,
     Target: Target,
-    Dist: Dist,
     BuildTarget: BuildTarget,
-    tarFromZip: tarFromZip,
     googleFonts: googleFonts,
-    fileDownload: fileDownload
+    fileDownload: fileDownload,
+    zipFile: zipFile,
+    zipFileDownload: zipFileDownload,
+    localNpmPackage: localNpmPackage,
+    pythonVenv: pythonVenv,
+    pythonDocs: pythonDocs,
+    formatSpec: formatSpec,
+    inject: inject,
+    dist: dist
 };
--- _site/configure.js
+++ _site/configure.js
@@ -1,136 +1,27 @@
 var path = require("path");
 var fs = require("fs");
-var os = require("os");
 var builder = require("./builder/builder");
 var glob = require("glob");
-var nodeExec = "node";
 var bin = "node_modules/.bin";
 
 
-var copyFromArchive = function (name) {
-    var source = `_site/archive/${name}.tar`;
-    var archive = `archive-${name}`;
-    var filename = `build/${name}`;
-
-    return new builder.Target({
-        filename: filename,
-        staticDeps: [source],
-        commands: [
-            `mkdir -p build`,
-            `cp -R ${source} ${filename}`
-        ]
-    });
-};
-
-var pythonBuildVenv = new builder.BuildTarget({
-    salt: 'virtualenv',
-    getCommands: function (tmp) {
-        return `
-            python3 -m venv --without-pip ${tmp}
-            wget https://bootstrap.pypa.io/get-pip.py -P ${tmp}
-            ${tmp}/bin/python ${tmp}/get-pip.py
-            ${tmp}/bin/pip install sphinx
-        `;
-    }
-});
-
-var pythonDocs = function (version) {
-    var staticDeps = glob.sync(`python/${version}/docs/**`);
-    staticDeps = staticDeps.concat(glob.sync(`python/${version}/teleport/**`));
-    staticDeps = staticDeps.concat(`python/${version}/CHANGES.rst`);
-
-    return new builder.BuildTarget({
-        resultDir: "/python/out",
-        staticDeps: staticDeps,
-        mounts: {
-            "/flask-sphinx-themes": builder.tarFromZip("https://github.com/cosmic-api/flask-sphinx-themes/archive/master.zip"),
-            "/intersphinx/python2": builder.fileDownload({
-                filename: "python2.inv",
-                url: "https://docs.python.org/2.7/objects.inv"
-            }),
-            "/venv": pythonBuildVenv
-        },
-        getCommands: function (tmp) {
-            return `
-                cp -R python/${version} ${tmp}/python
-                cp ${tmp}/intersphinx/python2/python2.inv ${tmp}/python/docs
-                echo '\\nhtml_theme_path = ["../../flask-sphinx-themes/flask-sphinx-themes-master"]\\n' >> ${tmp}/python/docs/conf.py
-                echo '\\nintersphinx_mapping = {"python": ("http://docs.python.org/2.7", "python2.inv")}\\n' >> ${tmp}/python/docs/conf.py
-                (cd ${tmp}/python; ../venv/bin/python setup.py install)
-                (cd ${tmp}/python; ../venv/bin/python ../venv/bin/sphinx-build -b html -D html_theme=flask docs out)
-            `;
-        }
-    });
-};
-
-var formatSpec = function (source) {
-    return new builder.BuildTarget({
-        staticDeps: [
-            "_site/spec.js",
-            "_site/templates/spec.mustache",
-            `_spec/${source}.xml`
-        ],
-        resultDir: "/out",
-        getCommands: function (tmp) {
-            return `
-                xml2rfc --no-network _spec/${source}.xml --text --out=${tmp}/teleport.txt
-                mkdir ${tmp}/out
-                ${nodeExec} _site/spec.js < ${tmp}/teleport.txt > ${tmp}/out/index.html
-            `;
-        }
-    });
-};
-
-var inject = function (options) {
-    var src = options.src;
-    var args = options.args;
-    return new builder.BuildTarget({
-        staticDeps: ["_site/inject.js", "_site/templates/navbar.mustache"],
-        mounts: {
-            "/": src
-        },
-        getCommands: function (tmp) {
-            return `find ${tmp} -iname \\*.html | xargs ${nodeExec} _site/inject.js ${args}`;
-        }
-    });
-};
-
-var rootDir = path.join(__dirname, "..");
-var makefile = new builder.Makefile(rootDir);
-var deployTmp = "dist";
-
-makefile.addTask(
-    "deploy",
-    `
-        rm -rf ${deployTmp}
-        mkdir -p ${deployTmp}
-        tar xf build/site.tar -C ${deployTmp}
-        ${bin}/surge --project ./dist --domain www.teleport-json.org
-    `
-);
-
-makefile.addTask("clean", "rm -rf build/*");
-
-var localNpmPackage = function (name) {
-    return new builder.BuildTarget({
-        staticDeps: [`node_modules/${name}/package.json`],
-        getCommands: function (tmp) {
-            return `cp -R node_modules/${name}/* ${tmp}`;
-        }
-    });
-};
+var pythonBuildVenv = builder.pythonVenv("3.5");
 
 var bootstrap = new builder.BuildTarget({
     resultDir: "/dist",
+    archive: 'bootstrap-modified',
     mounts: {
-        "/": builder.tarFromZip("https://github.com/twbs/bootstrap/releases/download/v3.3.0/bootstrap-3.3.0-dist.zip"),
+        "/": builder.zipFileDownload(
+            "bootstrap-dist",
+            "https://github.com/twbs/bootstrap/releases/download/v3.3.0/bootstrap-3.3.0-dist.zip"),
         "/fonts": builder.googleFonts("http://fonts.googleapis.com/css?family=Lato:400,700,400italic|Inconsolata:400,700"),
-        "/lumen": builder.fileDownload({
-            filename: "bootstrap-lumen.css",
-            url: "http://bootswatch.com/flatly/bootstrap.css"
-        }),
-        "/highlight": localNpmPackage("highlight.js"),
-        "/awesome": builder.tarFromZip("http://fortawesome.github.io/Font-Awesome/assets/font-awesome-4.5.0.zip")
+        "/lumen": builder.fileDownload(
+            "bootstrap-lumen.css",
+            "http://bootswatch.com/flatly/bootstrap.css"),
+        "/highlight": builder.localNpmPackage("highlight.js"),
+        "/awesome": builder.zipFileDownload(
+            "font-awesome",
+            "http://fortawesome.github.io/Font-Awesome/assets/font-awesome-4.5.0.zip")
     },
     staticDeps: ["_site/static/static.css"],
     getCommands: function (tmp) {
@@ -160,67 +51,44 @@ var bootstrap = new builder.BuildTarget({
 });
 
 var site = new builder.BuildTarget({
+    archive: 'site',
     staticDeps: [
         "_site/static",
         "_site/index.js",
-        "_site/templates/index.mustache",
         "_site/inject.js",
+        "_site/templates/index.mustache",
         "_site/templates/navbar.mustache"
     ],
     mounts: {
         "/static/bootstrap": bootstrap,
-
-        // "/python/0.5": inject({
-        //     src: pythonDocs("0.5"),
-        //     args: "--navbar 'python/0.5' --bs"
-        // }),
-
-        "/python/0.4": inject({
-            src: pythonDocs("0.4"),
-            args: "--navbar 'python/0.4' --bs"
-        }),
-
-        "/python/0.3": inject({
-            src: pythonDocs("0.3"),
-            args: "--navbar 'python/0.3' --bs"
-        }),
-
-        "/python/0.2": inject({
-            src: pythonDocs("0.2"),
-            args: "--navbar 'python/0.2' --bs"
-        }),
-
-        "/spec/draft-00": inject({
-            src: formatSpec("draft-00"),
-            args: "--navbar 'spec/draft-00' --bs"
-        }),
-
-        "/spec/draft-01": inject({
-            src: formatSpec("draft-01"),
-            args: "--navbar 'spec/draft-01' --bs"
-        }),
-
-        "/spec/draft-02": inject({
-            src: formatSpec("draft-02"),
-            args: "--navbar 'spec/draft-02' --bs"
-        }),
-
-        "/spec/draft-03": inject({
-            src: formatSpec("draft-03"),
-            args: "--navbar 'spec/draft-03' --bs"
-        }),
-
-        "/spec/draft-04": inject({
-            src: formatSpec("draft-04"),
-            args: "--navbar 'spec/draft-04' --bs"
-        }),
-
-        "/spec/1.0": inject({
-            src: copyFromArchive("spec-old"),
-            args: "--navbar 'spec/1.0' --bs"
-        }),
-
-        "/npm-jquery": localNpmPackage("jquery")
+        "/python/0.4": builder.inject(
+            builder.pythonDocs("0.4", pythonBuildVenv),
+            "--navbar 'python/0.4' --bs"),
+        "/python/0.3": builder.inject(
+            builder.pythonDocs("0.3", pythonBuildVenv),
+            "--navbar 'python/0.3' --bs"),
+        "/python/0.2": builder.inject(
+            builder.pythonDocs("0.2", pythonBuildVenv),
+            "--navbar 'python/0.2' --bs"),
+        "/spec/draft-00": builder.inject(
+            builder.formatSpec("draft-00"),
+            "--navbar 'spec/draft-00' --bs"),
+        "/spec/draft-01": builder.inject(
+            builder.formatSpec("draft-01"),
+            "--navbar 'spec/draft-01' --bs"),
+        "/spec/draft-02": builder.inject(
+            builder.formatSpec("draft-02"),
+            "--navbar 'spec/draft-02' --bs"),
+        "/spec/draft-03": builder.inject(
+            builder.formatSpec("draft-03"),
+            "--navbar 'spec/draft-03' --bs"),
+        "/spec/draft-04": builder.inject(
+            builder.formatSpec("draft-04"),
+            "--navbar 'spec/draft-04' --bs"),
+        "/spec/1.0": builder.inject(
+            builder.zipFile("spec-old", "_site/archive/spec-old.zip"),
+            "--navbar 'spec/1.0' --bs"),
+        "/npm-jquery": builder.localNpmPackage("jquery")
     },
 
     getCommands: function (tmp) {
@@ -228,20 +96,31 @@ var site = new builder.BuildTarget({
             cp -R _site/static ${tmp}
             cp ${tmp}/npm-jquery/dist/jquery* ${tmp}/static
             rm -rf ${tmp}/npm-jquery
-            ${nodeExec} _site/index.js > ${tmp}/index.html
-            ${nodeExec} _site/inject.js ${tmp}/index.html --navbar '/' --bs --highlight
+            node _site/index.js > ${tmp}/index.html
+            node _site/inject.js ${tmp}/index.html --navbar '/' --bs --highlight
         `;
     }
 });
 
-makefile.addRule(new builder.Dist('site', site));
-makefile.addRule(new builder.Dist('site-ga', inject({
-    src: site,
-    args: "--analytics"
-})));
+var siteGA = builder.inject(site, "--analytics");
+
+var siteDist = builder.dist('site', site);
+var siteDistGA = builder.dist('site-ga', siteGA);
+
+var rootDir = path.join(__dirname, "..");
+var makefile = new builder.Makefile(rootDir);
+
+makefile.addRule(siteDist);
+makefile.addRule(siteDistGA);
+
+makefile.addTask("clean", "rm -rf build/*");
+makefile.addTask(
+    "deploy",
+    `${bin}/surge --project ${siteDistGA.getFilename()} --domain www.teleport-json.org`
+);
 
 if (require.main === module) {
-    fs.writeFileSync(`${__dirname}/../Makefile`, makefile.toString())
+    fs.writeFileSync(`${rootDir}/Makefile`, makefile.toString())
 }
 
 module.exports = {
